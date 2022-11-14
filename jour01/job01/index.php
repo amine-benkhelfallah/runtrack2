2a8048d17d9a2d482b4f2e9c923bd8234d969f60
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-11-14 17:35:44 +0100

    Variable PHP

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -7,35 +7,38 @@
     <body>
       
         <?php
-        $str2 = ' vive ';
-        echo $str2;
 
-        $str = 'LaPlateforme';
-        echo $str;
-      
-        $str3 = ' ! ';
-        echo $str3; 
-     
-        echo "<br>";  // saut de ligne 
-        $val = '6';
-        echo $val; // afficher la variable 6
+        /************ variable str affichage  *******/
+            $str2 = ' vive ';
+            echo $str2;
 
-        echo "<br>"; // saut deligne 
-        echo $val + 4; //ajouter a notre variable et l'afficher 
-        echo "<br>"; // saut deligne 
-
+            $str = 'LaPlateforme';
+            echo $str;
         
+            $str3 = ' ! ';
+            echo $str3; 
+        
+            echo "<br>";  // saut de ligne 
+        /***** variable val == 6 + addition *******/
+            $val = '6';
+            echo $val; // afficher la variable 6
+
+            echo "<br>"; // saut deligne 
+            echo $val + 4; //ajouter a notre variable et l'afficher 
+        
+        /***** variable myBool true or false ******/
+            echo "<br>"; // saut deligne 
+
+            
             $myBool = true;
             echo $myBool;  //  afficher 1
-            
+                
             echo "<br>"; // saut deligne 
 
             $myBool = false;
             echo $myBool; // afficher 0 
 
-           
-    
+        ?>
 
-            ?>
     </body>
 </html>
