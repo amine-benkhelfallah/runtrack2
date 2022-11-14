fbce6e363f4cb805002427b606fb36f3409ff353
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-11-14 13:05:34 +0100

    echo en PHP Hello LaPlateforme!

---
+++ jour01/job00/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours PHP & MySQL</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+      
+        <?php
+
+            echo '“Hello LaPlateforme!”';
+            ?>
+    </body>
+</html>
