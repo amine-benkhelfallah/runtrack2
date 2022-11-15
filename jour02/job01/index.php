4134dfeb228110a8c442596d2a0b89a5768c7f96
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-11-15 13:23:08 +0100

    les conditions en PHP

---
+++ jour02/job01/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP jour 2 </title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+    
+        <?php
+            for($x = 1; $x <= 1336; $x++){   
+                if ($x == 42) {
+                    echo "<b><u> $x </u></b> <br>";
+                } 
+                else {
+                    echo "$x <br>";
+                }     
+            }
+            ?>
+           
+    </body>
+</html>
+
+    
+
+    </body>
+</html>
