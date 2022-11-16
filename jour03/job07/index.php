d466b7f125a213204c2a6019da8c159eefad87a3
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-11-16 21:22:09 +0100

    PHP,Les tableaux et modifications de chaine de caractère

---
+++ jour03/job07/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html >
+    <head>
+        <title>PHP jour 3  job7</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+
+        <?php
+
+
+
+           $str = "Certaines choses changent, et d'autres ne changeront jamais";
+            $str2 = "";
+    
+           for($i = 0; isset($str[$i]); $i++){
+
+                if (!isset($str[$i+1])){
+                    $str2 = $str2.$str[0];
+                }
+                else{
+                    $str2 = $str2.$str[$i+1];
+                }
+                
+           }
+           
+           echo $str2;
+    
+        ?>
+    
+
+    </body>
+</html>
