113271ea0c19cc7b3de4a8eadd0db69d6ca5e6a4
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-11-17 13:55:30 +0100

    PHP, Algorithme qui affiche le nombre d’arguments

---
+++ jour04/job01/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP jour 4 job1</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+    
+       
+        
+         <form action="action.php " method="GET"> </br>
+         <input type="text" name="name" placeholder="name"> </br>
+         <textarea name="message" placeholder="Message"></textarea>  </br>
+         <input type="submit" value="Envoyer!">
+       </form>
+   
+     
+           
+    </body>
+</html>
+
+    
