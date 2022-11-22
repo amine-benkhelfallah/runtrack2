769d6534967995ff0f8e81f7ef4707a8a5c62365
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-11-22 11:20:24 +0100

    fonction getHello()/ retourne HelloLaplateforme! puis affichage avec echo

---
+++ jour07/job03/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html >
+    <head>
+        <title>PHP jour 7  job3</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+
+
+        <?php
+        function getHello(){
+            return "Hello LaPlateforme!";
+        }
+
+        echo getHello();
+
+        ?>
+    
+
+
+    </body>
+</html> 
