42e29f1a69c8594c2060f6ca0c8cef9efb5f6d7a
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-11-22 10:42:36 +0100

    fonction hello() appelt affiche Hello Laplateforme!

---
+++ jour07/job01/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html >
+    <head>
+        <title>PHP jour 7  job1</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+
+
+
+            <?php
+             function hello($nom){
+                echo "Hello $nom!";
+
+             }
+ 
+            hello("LaPlateforme");
+
+            ?>
+    </body>
+</html> 
