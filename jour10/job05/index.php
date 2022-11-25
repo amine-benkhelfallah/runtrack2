1dfbe8a071e4c8d0696cb5f8caf7db1868d6ce07
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2022-11-25 14:30:10 +0100

    PHP + SQL

---
+++ jour10/job05/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+        <title>jour 10 job05</title>
+	</head>
+	<body>
+    <h1>Base de données “jour09”</h1>
+        <?php
+        $mysqli = new mysqli("localhost", "root", "", "jour09");
+        $mysqli->set_charset("utf8");
+        $request = $mysqli->query("SELECT `etudiants`.* FROM `etudiants` WHERE TIMESTAMPDIFF(YEAR, `naissance`, CURDATE()) <= 18; ");
+        $result_fetch_arry = $request -> fetch_array(MYSQLI_ASSOC);
+
+        //<thead>
+        echo "<table border='2'><tr>";
+        foreach ( $result_fetch_arry as $key => $value)
+        {
+        echo " <th> " . $key . " </th> ";
+        } 
+        echo "</tr>";
+        //</thead>
+        //<tbody>
+        while ( $result_fetch_arry != NULL)
+        {
+            echo "<tr>";
+            foreach ( $result_fetch_arry as $value)
+            {
+                echo "<td>" . $value . "</td>";
+            }
+            echo "</tr>";
+            $result_fetch_arry = $request -> fetch_array(MYSQLI_ASSOC);
+        }
+        echo "</table>";
+         //</tbody>
+        ?>   
+    </body>
+</html>
+
+
+
+
